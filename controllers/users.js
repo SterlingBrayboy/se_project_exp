e2866d82fc48f5d641c729c5aaa600067fcf2ccc
LEARNER <sterlingbrayboyn@gmail.com>
2024-06-13 00:39:29 -0400

    User Controllers

--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,38 @@ const router = require("../routes");
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => {
-      throw Error("AHHH!");
-      res.send(users);
-    })
+    .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
+      return res.status(500).send({ message: err.message });
     });
 };
 
-module.exports = { getUsers };
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "") {
+        // return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+      } else if (err.name === "CastError") {
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
