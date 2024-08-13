e4937972488768353d1b78fcc1002fabf14a3b31
LEARNER <sterlingbrayboyn@gmail.com>
2024-08-13 02:35:58 -0400

    Fixing MODULE_NOT_FOUND

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ mongoose
   .catch(console.error);
 
 const routes = require("./routes");
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 
 // app.use(express.json());
 // app.use("/", mainRouter);
