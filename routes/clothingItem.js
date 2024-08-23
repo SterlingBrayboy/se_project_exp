98d09d21f97496db9f1d36e4337649ce02284d82
LEARNER <sterlingbrayboyn@gmail.com>
2024-08-23 02:54:17 -0400

    Create, GET, & Update

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
 
-const { createItem } = require("../controllers/clothingItems");
+const {
+  createItem,
+  getItems,
+  updateItem,
+} = require("../controllers/clothingItems");
+const clothingItem = require("../models/clothingItem");
 
 //CRUD
 
 //Create
 router.post("/", createItem);
+
+//Read
+
+router.get("/", getItems);
+
+//Update
+
+router.put("/:itemId", updateItem);
+
+//Delete
 
 module.exports = router;
