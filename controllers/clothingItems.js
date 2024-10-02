8b63c8770566a91ec593338c61c863ce6add804a
LEARNER <sterlingbrayboyn@gmail.com>
2024-10-02 03:57:07 -0400

    Expected Return Test #3

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -129,15 +129,17 @@ const unlikeItem = (req, res) => {
       if (!item) {
         return res.status(NOT_FOUND_CODE).send({ message: "Item not found" });
       }
-      res.status(200).send({ data: item });
+      return res.status(200).send({ data: item });
     })
     .catch((err) => {
       if (err.name === "CastError") {
         // 400 — invalid data passed to the methods
-        res.status(BAD_REQUEST_CODE).send({ message: "Invalid item ID" });
+        return res
+          .status(BAD_REQUEST_CODE)
+          .send({ message: "Invalid item ID" });
       } else if (err.statusCode === NOT_FOUND_CODE) {
         // 404 — the requested ID or URL doesn't exist
-        res.status(NOT_FOUND_CODE).send({ message: err.message });
+        return res.status(NOT_FOUND_CODE).send({ message: err.message });
       }
     });
 };
