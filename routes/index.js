4a7b02a7d6fe6d5e361063b084f1fff0f772737c
LEARNER <sterlingbrayboyn@gmail.com>
2024-10-04 04:18:50 -0400

    REMOVE NODE_MODULES FOLDER

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const userRouter = require("./users");
 const { NOT_FOUND_CODE } = require("../utils/errors");
 
 router.use("/items", clothingItem);
-// router.use("/likes", userRouter);
 router.use("/users", userRouter);
 router.use((req, res) => {
   res.status(NOT_FOUND_CODE).send({ message: "Router not found" });
