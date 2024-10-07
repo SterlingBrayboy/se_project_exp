4224040e6336a1887e96569eb79cf519597ec966
LEARNER <sterlingbrayboyn@gmail.com>
2024-10-07 07:00:09 -0400

    Create Login Controller (commented out)

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,12 @@
 const User = require("../models/user");
+// const jwt = require("jsonwebtoken");
+
 const {
   BAD_REQUEST_CODE,
   NOT_FOUND_CODE,
   INTERNAL_SERVICE_ERROR_CODE,
+  // UNAUTHORIZED_CODE,
 } = require("../utils/errors");
-
-// GET /users
 
 const getUsers = (req, res) => {
   User.find({})
@@ -52,5 +53,34 @@ const getUser = (req, res) => {
         .send({ message: "Internal Service Error" });
     });
 };
+
+// userSchema.statics.findUserByCredentials = function (email, password) {
+//   return this.findOne({ email }).then((user) => {
+//     if (!user) {
+//       return Promise.reject(new Error('Incorrect email or password'));
+//     }
+//     // Assume you use bcrypt to check password
+//     return bcrypt.compare(password, user.password).then((matched) => {
+//       if (!matched) {
+//         return Promise.reject(new Error('Incorrect email or password'));
+//       }
+//       return user;
+//     });
+//   });
+// };
+
+// const login = (req, res) => {
+// const { email, password } = req.body;
+//   User.findUserByCredentials(email, password)
+//     .then((user) => {
+//        const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+//        expiresIn: "7d",
+//        res.send({ token });
+//        });
+//      .catch((err) => {
+//       console.error(err);
+//       return res.status(UNAUTHORIZED_CODE).send({ message: "Unauthorization Error"});
+//     });
+// };
 
 module.exports = { getUsers, createUser, getUser };
